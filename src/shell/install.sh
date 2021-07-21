d6223164fe530a7111269a04d0fed2231fa6656b
Brave Cobra <bravecobra@gmail.com>
2021-07-21 15:43:00 +0200

    Added ArgoCD to shell

--- src/shell/install.sh
+++ src/shell/install.sh
@@ -1,18 +1,36 @@
-$traefik_helm_version = "9.19.1" # 10.1.1
-$certmanager_helm_version = "v1.3.1" # v1.4.0
-$jaeger_helm_version= "2.21.1" # 2.23.0
-$prometheus_helm_version= "16.0.1" # 16.15.0
-$loki_helm_version= "2.5.0" # 2.5.3
-$promtail_helm_version= "3.5.1" # 3.6.0
-$consul_helm_version= "0.31.1" # 0.32.1
-$vault_helm_version = "0.13.0" # 0.13.0
-$elastic_helm_version = "1.6.0" # 1.6.0
-$identityserver4admin_helm_version = "0.4.0" # 0.4.0
+#!/bin/bash
 
-Copy-Item $env:LOCALAPPDATA\mkcert\rootCA.pem ./src/certs/cacerts.crt
-Copy-Item $env:LOCALAPPDATA\mkcert\rootCA-key.pem ./src/certs/cacerts.key
+# For using WSL's kubectl copy the context to the default kubectl context
+# cp /mnt/c/users/<username>/.kube/config ~/.kube/config
+
+# install mkcert in ubuntu
+# https://computingforgeeks.com/how-to-create-locally-trusted-ssl-certificates-on-linux-and-macos-with-mkcert/
+
+traefik_helm_version=9.19.1 # 10.1.1
+certmanager_helm_version=v1.3.1 # v1.4.0
+jaeger_helm_version=2.21.1 # 2.23.0
+prometheus_helm_version=16.0.1 # 16.15.0
+loki_helm_version=2.5.0 # 2.5.3
+promtail_helm_version=3.5.1 # 3.6.0
+consul_helm_version=0.31.1 # 0.32.1
+vault_helm_version=0.13.0 # 0.13.0
+lastic_helm_version=1.6.0 # 1.6.0
+identityserver4admin_helm_version=0.4.0 # 0.4.0
+argocd_helm_version=3.10.0 # 3.10.0
+
+if grep -q microsoft /proc/version; then
+  echo "Ubuntu on Windows"
+  localappdata=`wslpath "$(wslvar LOCALAPPDATA)"`
+else
+  echo "native Linux"
+  localappdata=`~/.local/share`
+fi
+
+localappdata=`wslpath "$(wslvar LOCALAPPDATA)"`
+cp $localappdata/mkcert/rootCA.pem ./src/certs/cacerts.crt
+cp $localappdata/mkcert/rootCA-key.pem ./src/certs/cacerts.key
+
 kubectl create secret tls ca-key-pair --namespace=cert-manager --cert=./src/certs/cacerts.crt --key=./src/certs/cacerts.key  --dry-run=client -o yaml > ./src/argocd/argo/cert-manager/crds/cacerts.yaml
-
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 helm repo add bitnami https://charts.bitnami.com/bitnami
@@ -24,9 +42,10 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add elastic https://helm.elastic.co
 helm repo add identityserver4admin https://bravecobra.github.io/identityserver4.admin-helm/charts/
 helm repo add jetstack https://charts.jetstack.io
+helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
-##Creating namespaces
+## Creating namespaces
 kubectl create namespace cert-manager
 kubectl create namespace traefik
 kubectl create namespace consul
@@ -44,7 +63,7 @@ helm upgrade --install --wait cert-manager jetstack/cert-manager -n cert-manager
 
 kubectl apply -f ./src/shell/cert-manager/crds/cacerts.yaml
 kubectl apply -f ./src/shell/cert-manager/crds/cluster-issuer.yaml
-Start-Sleep -Seconds 10
+sleep 10s
 
 ## install traefik as ingress-controller
 helm upgrade --install --wait traefik traefik/traefik -n traefik --version $traefik_helm_version -f ./src/shell/traefik/traefik-values.yaml
@@ -60,7 +79,7 @@ kubectl apply -f ./src/shell/coredns/coredns.yaml
 ## Prometheus
 helm upgrade --install --wait prometheus prometheus-community/kube-prometheus-stack -n prometheus --version $prometheus_helm_version -f ./src/shell/prometheus/prometheus-values.yaml
 kubectl apply -f ./src/shell/prometheus/crds/
-kubectl apply -f ./src/shell/prometheus/crds/dashboards/
+kubectl apply $(ls ./src/shell/prometheus/crds/dashboards/*.yaml | awk '{ print " -f " $1 }')
 
 ## Loki
 helm upgrade --install --wait loki grafana/loki -n loki --version $loki_helm_version -f ./src/shell/loki/loki-values.yaml
@@ -85,17 +104,17 @@ helm upgrade --install --wait identityserver4 identityserver4admin/identityserve
 
 ## install argocd
 helm upgrade --install --wait argocd argo/argo-cd -n argocd --version $argocd_helm_version  -f ./src/shell/argocd/argo-values.yaml
-$argopass = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-Write-Host "ArgoCD username: admin"
-Write-Host "ArgoCD password: $argopass"
+argopass=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+echo "ArgoCD username: admin"
+echo "ArgoCD password: $argopass"
 # argocd login argo.k8s.local:443 --grpc-web --username admin --password $argopass
 # argocd app create root --grpc-web --repo=https://github.com/bravecobra/k8s-dev-infrastructure --path=src/argocd/infrastructure/root/root-app --revision=develop --dest-server=https://kubernetes.default.svc --dest-namespace=default --self-heal --auto-prune --sync-option CreateNamespace=true --sync-policy=automated
 # argocd app create infrastructure --grpc-web --repo=https://github.com/bravecobra/k8s-dev-infrastructure --path=src/argocd/infrastructure/base/infrastructure-app --revision=develop --dest-server=https://kubernetes.default.svc --dest-namespace=infrastructure --self-heal --auto-prune --sync-option CreateNamespace=true --sync-policy=automated
 
-$elasticsearchpass = kubectl -n elasticsearch get secret elastic-es-es-elastic-user -o jsonpath='{.data.elastic}' | base64 -d
-Write-Host "Elasticsearch username: elastic"
-Write-Host "Elasticsearch password: $elasticsearchpass"
-$consultoken = kubectl -n consul get secret consul-consul-bootstrap-acl-token -o jsonpath="{.data.token}" | base64 -d
-$consuldns = kubectl get svc consul-consul-dns -o jsonpath='{.spec.clusterIP}' --namespace=consul
-Write-Host "Consul DNS Server ClusterIP: $consuldns"
-Write-Host "Consul bootstrap root token: $consultoken"
+elasticsearchpass=`kubectl -n elasticsearch get secret elastic-es-es-elastic-user -o jsonpath='{.data.elastic}' | base64 -d`
+echo "Elasticsearch username: elastic"
+echo "Elasticsearch password: $elasticsearchpass"
+consultoken = kubectl -n consul get secret consul-consul-bootstrap-acl-token -o jsonpath="{.data.token}" | base64 -d
+consuldns = kubectl get svc consul-consul-dns -o jsonpath='{.spec.clusterIP}' --namespace=consul
+echo "Consul DNS Server ClusterIP: $consuldns"
+echo"Consul bootstrap root token: $consultoken"
