e250aac1d8858997c93110e84b54f346207c9500
Brave Cobra <bravecobra@gmail.com>
2021-11-17 12:51:19 +0100

    Added k3s/k3d through terraform

---
+++ src/clusters/k3d/variables.tf
@@ -0,0 +1,43 @@
+variable "cluster-name" {
+  type = string
+}
+
+variable "server-node-count" {
+  type = number
+  default = 1
+}
+
+variable "worker-node-count" {
+  type = number
+  default = 0
+}
+
+variable "install_private_registry" {
+  type = bool
+  default = true
+}
+
+variable "k3s-version" {
+  description = "The K3s version to use"
+  type        = string
+  default     = "latest"
+}
+
+variable "k3d-cluster-name" {
+  default = ["k3d"]
+  type    = list(string)
+}
+
+variable "k3d-cluster-domain" {
+  type    = string
+}
+
+variable "k3d-cluster-port" {
+  default = 6445
+  type    = number
+}
+
+variable "k3d-cluster-ip" {
+  default = "0.0.0.0"
+  type    = string
+}
